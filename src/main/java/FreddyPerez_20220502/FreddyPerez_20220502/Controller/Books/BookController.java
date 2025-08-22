c57b31a70b297e6b7fe2cac0d959441b48770e68
Freddy Perez <20220502ricaldone.edu.sv>
2025-08-22 15:13:28 -0600

    Proyecto Terminado

--- src/main/java/FreddyPerez_20220502/FreddyPerez_20220502/Controller/Books/BookController.java
+++ src/main/java/FreddyPerez_20220502/FreddyPerez_20220502/Controller/Books/BookController.java
@@ -13,31 +13,36 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @RestController
 @RequestMapping("/apiBook")
 public class BookController {
+    //Inyectamos el Service de Libro
     @Autowired
     private BookService service;
 
+    //Metodo que sirve para buscar todos los libros
     @GetMapping("/getBooks")
     public List<BookDTO> getBook(){
+        //Retornamos el metodo del servuice
         return service.getBooks();
     }
 
+    //Metodo que sirve para buscar un libro mediante su ID
     @GetMapping("/getBookById/{id}")
-    public List<BookDTO> getBookById(@PathVariable Long id){
-        return service.getBookById();
+    public Optional<BookDTO> getBookById(@PathVariable Long id){
+        //Retornamos el metodo del  service
+        return service.getBookById(id);
     }
 
+    //Metodo que sirve para insertar un  libro, hay que tener almenos 1 autor para poder insertar un libro
     @PostMapping("/insertBook")
     public ResponseEntity<Map<String, Object>> insertBook(@Valid @RequestBody BookDTO dto, HttpServletRequest request){
         try{
+            //Creamos un objeto de tipo DTO para almacenar el resultado del metodo del service
             BookDTO answer = service.insertBook(dto);
+            //Si la respuesta es nula mandamos un BAD REQUEST
             if(answer == null){
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
                    "status", "error",
@@ -45,12 +50,14 @@ public class BookController {
                         "mesage", "No ha sido posible insertar un libro"
                 ));
             }
+            //Si sale bien mandamos una respuesta que se pudo crear el libro
             return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
                 "status", "success",
                     "data", answer
             ));
         }
         catch (Exception e){
+            //Si hay algun error mandamos un el mensaje con el motivo del error mediante un INTERNAL SERVER ERROR
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
                     "status", "error",
                     "message", "Error de insercion",
@@ -59,16 +66,21 @@ public class BookController {
         }
     }
 
+    //Metodo que sirve para actualizar un libro mediante su ID
     @PutMapping("/updateBook/{id}")
     public ResponseEntity<?> updateBook(@PathVariable Long id, @Valid @RequestBody BookDTO dto, BindingResult bindingResult){
+        //Mediante un objeto de tipo binding result evaluamos si hay alguun error, si es que lo hay mandamos un HashMap con el cuerpo de errores mediante un badRequest
         if(bindingResult.hasErrors()){
             Map<String, String > errors = new HashMap<>();
             bindingResult.getFieldErrors().forEach(error ->
                     errors.put(error.getField(), error.getDefaultMessage()));
             return ResponseEntity.badRequest().body(errors);
         }
+        //Si sale bien intentamos:
         try{
+            //Crear un objeto de tipo DTO que almacene el resultado del metodo del service
             BookDTO answer = service.updateBook(id, dto);
+            //Si la respuesta es nula mandamos un BAD REQUEST
             if(answer == null){
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
                     "status", "error",
@@ -76,12 +88,14 @@ public class BookController {
                         "message", answer
                 ));
             }
+            //Si sale bien mandamos una respuesta que se pudo actualizar el libro
             return ResponseEntity.status(HttpStatus.ACCEPTED).body(Map.of(
                     "status", "success",
                     "data", answer
             ));
         }
         catch (Exception e){
+            //Si hay algun error mandamos un el mensaje con el motivo del error mediante un INTERNAL SERVER ERROR
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
                     "status", "error",
                     "message", "Error de actualizacion",
@@ -90,22 +104,27 @@ public class BookController {
         }
     }
 
+    //Metodo que sirve para eliminar un un libro mediante su ID
     @DeleteMapping("/deleteBook/{id}")
     public ResponseEntity<Map<String, Object>> deleteBook(@PathVariable Long id){
         try{
+            //Evaluamos si exite el ID del libro, si no llega a existir
             if(!service.deleteBook(id)){
+                //Mandamos un cuerpo de mensaje mediante un NOT FOUND que dice que no exite el libro, ya que la respuesta fue un false
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
                     "error", "not found",
                         "mesaage", "id no encontrado",
                         "timestamp", Instant.now().toString()
                 ));
             }
+            //Si existe y se pudo eliminar mandamos un cuerpo de mensaje mediante un OK diciendo que la eliminacion fue exitosa
             return ResponseEntity.status(HttpStatus.OK).body(Map.of(
                 "status", "process completed",
                     "message", "Proceso completado con exito"
             ));
         }
         catch (Exception e){
+            //Si hay algun error mandamos un el mensaje con el motivo del error mediante un INTERNAL SERVER ERROR
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
                     "status", "error",
                     "message", "Error de eliminacion",
--- src/main/java/FreddyPerez_20220502/FreddyPerez_20220502/Services/Books/BookService.java
+++ src/main/java/FreddyPerez_20220502/FreddyPerez_20220502/Services/Books/BookService.java
@@ -8,89 +8,131 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service @Slf4j
 public class BookService {
+    //Inyectamos el repositorio de Libro
     @Autowired
     private BookRepository repo;
+    //Creamos un Metodo de Lista que sirve para buscar todos los los libros que existen
     public List<BookDTO> getBooks(){
+        //Mediante un objeto de tipo entity mandamos a llamar al metodo finAll del repository para encontrar todos los elementos del libro
         List<BookEntity> entity = repo.findAll();
+        //Retorno de datos convirtiendolos a DTO y mandandolos en formato de lista
         return entity.stream()
                 .map(this::BookToDTO)
                 .collect(Collectors.toList());
     }
 
+    //Metodo que sirve para buscar un libro mediante su ID
+    public Optional<BookDTO> getBookById(Long id){
+        //Mediante un objetod de tipo Entity buscamos el libro con el metodo de repository findById
+        Optional<BookEntity> entity = repo.findById(id);
+        //Retrono de un Map convirtiendo los datos a DTO
+        return entity.map(this::BookToDTO);
+    }
+
+    //Metdo que sirve para insertar un libro, recibiendo como parametros la Data
     public BookDTO insertBook(BookDTO dto){
+        //Si la data llega a ser nula
         if(dto == null){
+            //Mandamos una excepcion qie doga que la data no puede ser nula o vacia
             throw new IllegalArgumentException("Book cant be null or empty");
         }
         try{
+            //Creamos un objeto de tipo entity que tenga como valor los valores de la data del dto, convirtiendolos antes a datos Entity
             BookEntity entity = DTOToBook(dto);
 
+            //Creamos un objeto de tipo entity que tenga la respuesta y los datos del metodo repo.save ocn parametro de entity
             BookEntity savedEntity = repo.save(entity);
+            //Retornamos el metodo de convertir a DTO como parametro savedEntity
             return BookToDTO(savedEntity);
         }
         catch (Exception e){
+            //Excepcion diciendo que no ha sido posible crear un libro
             throw new RuntimeException("Cant create book" + e.getMessage());
         }
     }
 
+    //Metodo que sirve para actualizar un libro, obtendiendo com parametros los Id y la data
     public BookDTO updateBook(Long id, BookDTO dto){
         try{
+            //Evaluamos si eñ ID existe, si sí existe
             if(repo.existsById(id)){
+                //Creamos un objeto de tipo entity que almacene el metodo que obtiene el id, como param,etro el id que nosotros colocamos
                 BookEntity entity = repo.getById(id);
+                //Despues asignamos los valores de los SET del entity con los valores que tienen los GET del DTO
                 entity.setTitulo(dto.getTitulo());
                 entity.setIsbn(dto.getIsbn());
                 entity.setGenero(dto.getGenero());
                 entity.setAnio_publicacion(dto.getAnio_publicacion());
                 entity.setId_Autor(dto.getId_Autor());
 
+                //Creamos un objeto de tipo entity que almacene la respuetsa y data del metodo repo.save, que recibe de parametro todos los datos de entity
                 BookEntity savedEntity = repo.save(entity);
+                //retornamos el la data convertida a DTO
                 return BookToDTO(savedEntity);
             }
+            //Ecxepcion que dice que el id que se intenta actualizar no existe
             throw new IllegalArgumentException("The book with the id " + id + " doesnt exist");
         }
         catch (Exception e){
-            throw new RuntimeException("Cant create book"+ e.getMessage());
+            //Excepcion diciendo que no ha sido posible actualizar un libro
+            throw new RuntimeException("Cant update book"+ e.getMessage());
         }
     }
 
+    //Metodo que sirve para eliminar un libro recibiendo como para metro su ID
     public boolean deleteBook(Long id){
         try{
+            //Evaluamos si existe el ID, si sí existe:
             if(repo.existsById(id)){
+                //Mandamos a llamar al metdo de eliminacion que recibe cmo parametro el ID recibido con anterioridafd
                 repo.deleteById(id);
+                //Retornamos un true
                 return true;
             }
             else{
+                //Si nada funciona retornamos un False
                 return false;
             }
         }
         catch (EmptyResultDataAccessException e){
+            //Ecxepcion que dice que el id que se intenta eliminar no existe
             throw new EmptyResultDataAccessException("The book with the id " + id + " doesnt exist", 1);
         }
     }
 
+    //Metodo que sirve para convertir la data de Entity a DTO
     private BookDTO BookToDTO(BookEntity entity){
+        //Se crea un objeto de tipo DTO
         BookDTO dto = new BookDTO();
+        //Asignamos los valores que tiene eL entity a sus respestivos valores en el DTO
         dto.setId(entity.getId());
         dto.setTitulo(entity.getTitulo());
         dto.setIsbn(entity.getIsbn());
         dto.setGenero(entity.getGenero());
         dto.setAnio_publicacion(entity.getAnio_publicacion());
         dto.setId_Autor(entity.getId_Autor());
+        //Finalizando retornando la data convertida a DTO
         return dto;
     }
 
+    //Metodo que sirve para convertir la data de DTO a Entity
     private BookEntity DTOToBook(BookDTO dto){
+        //Se crea un objeto de tipo Entity
         BookEntity entity = new BookEntity();
 
+        //Asignamos los valores que tiene eL DTO a sus respestivos valores en el Entity
         entity.setId(dto.getId());
         entity.setTitulo(dto.getTitulo());
         entity.setIsbn(dto.getIsbn());
         entity.setGenero(dto.getGenero());
         entity.setAnio_publicacion(dto.getAnio_publicacion());
         entity.setId_Autor(dto.getId_Autor());
+        //Finalizando retornando la data convertida a DTO
         return entity;
     }
 }
